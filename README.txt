2dd252aa399aff82e98ec6f8569b81e56604f299
Levi Mota <levi.mota@gmail.com>
2012-03-18 21:00:10 -0300

    Renaming files

--- README.txt
+++ README.txt
@@ -1,13 +1,11 @@
 ===============================
-LM35 Thermometer Sensor Library
+LM35 Temperature Sensor Library
 ===============================
 
 
 How to install
 ==============
 
-. Unpack the archive and rename the result to 'LM35Thermometer'.  
-
-. Put it in the 'libraries' folder in your Arduino sketches area.  
-
+. Unpack the archive and rename the result to 'LM35'.
+. Put it in the 'libraries' folder in your Arduino sketches area.
 . Restart the Arduino IDE to make sure it sees the new files.
--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,15 +1,16 @@
-#include <LM35Thermometer.h>
+#include <LM35.h>
 
 #define pinoSensor 0 //Pino analógico em que estará conectado o sensor LM35
 #define TEMPO_ESPERA 1000 //Intervalo entre cada leitura do sensor LM35 em milisegundos! OBS: 1000ms = 1s
 
-Thermometer therm;
+LM35Sensor lm35;
 
 void setup() { 
   Serial.begin(9600); //Iniciando Serial
 }
 
 void loop() {
-  Serial.println(therm.read(pinoSensor));
+  lm35.read(pinoSensor);
+  Serial.println(lm35.getCelsius());
   delay(TEMPO_ESPERA);// Espera um tempo determinado em TEMPO_LEITURA (milisegundos)
 }
