1bd079ad0f77612bc63a3c064a55bd940362288b
Levi Mota <levi.mota@gmail.com>
2012-03-18 21:26:46 -0300

    Renaming files

---
+++ LM35.cpp
@@ -0,0 +1,80 @@
+/*
+  LM35.cpp - Temperature Sensor Library for use LM35 - implementation
+  Copyright (c) 2012 Levi Mota.  All right reserved.
+*/
+
+// include core Wiring API
+#if defined(ARDUINO) && ARDUINO < 100
+#include "WProgram.h"
+#else
+#include "Arduino.h"
+#endif
+
+// include this library's description file
+#include "LM35.h"
+
+const double DIVIDER_HIGH_RES = 9.309;
+const double DIVIDER_LOW_RES = 2.048;
+
+// Constructors /////////////////////////////////////////////////////////////////
+// Function that handles the creation and setup of instances
+LM35Sensor::LM35Sensor(void) {
+  // initialize this instance's variables
+  setSamples(500);	
+  setHighRes(false);
+}
+
+LM35Sensor::LM35Sensor(int pSamples) {
+  // initialize this instance's variables
+  setSamples(pSamples);
+  setHighRes(false);
+}
+
+LM35Sensor::LM35Sensor(int pSamples, bool pHighRes) {
+  // initialize this instance's variables
+  setSamples(pSamples);
+  setHighRes(pHighRes);
+}
+
+// Public Methods //////////////////////////////////////////////////////////////
+// Functions available in Wiring sketches, this library, and other libraries
+
+void LM35Sensor::setSamples(int pSamples) {
+  samples = pSamples;
+}
+
+void LM35Sensor::setHighRes(bool pHighRes) {
+  highRes = pHighRes;
+  if (highRes) {
+    divider = DIVIDER_HIGH_RES;
+    analogReference(HIGH_RES);
+  } else {
+    divider = DIVIDER_LOW_RES;
+    analogReference(LOW_RES);
+  }
+}
+
+void LM35Sensor::read(int port) {
+  double tempSum = 0.0;
+  for (int i = 0; i < samples; i++) {
+    tempSum = tempSum + (analogRead(port) / divider);
+  } 
+  celsius = tempSum / samples; 
+  fahrenheit = 1.8 * celsius + 32;
+  kelvin = celsius + 273.15;
+}
+
+double LM35Sensor::getCelsius(void) {
+  return celsius;
+}
+
+double LM35Sensor::getFahrenheit(void) {
+  return fahrenheit;
+}
+
+double LM35Sensor::getKelvin(void) {
+  return kelvin;
+}
+
+// Private Methods /////////////////////////////////////////////////////////////
+// Functions only available to other functions in this library
---
+++ LM35.h
@@ -0,0 +1,56 @@
+/*
+  LM35.h - Temperature Sensor Library for use LM35 - description
+  Copyright (c) 2012 Levi Mota.  All right reserved.
+*/
+
+// ensure this library description is only included once
+#ifndef LM35_h
+#define LM35_h
+
+// include types & constants of Wiring core API
+#if defined(ARDUINO) && ARDUINO < 100
+#include "WProgram.h"
+#else
+#include "Arduino.h"
+#endif
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#define HIGH_RES INTERNAL1V1
+#define LOW_RES DEFAULT
+#else
+#define HIGH_RES INTERNAL
+#define LOW_RES DEFAULT
+#endif
+
+
+// library interface description
+class LM35Sensor
+{
+  // user-accessible "public" interface
+  public:
+    // constructors
+    LM35Sensor(void);
+    LM35Sensor(int samples);
+    LM35Sensor(int samples, bool highRes);
+	// setters
+    void setSamples(int samples);
+    void setHighRes(bool highRes); 
+	// readers
+    void read(int port);
+	// getters
+    double getCelsius(void);
+	double getFahrenheit(void);
+	double getKelvin(void);
+
+  // library-accessible "private" interface
+  private:
+    bool highRes;
+	double divider;
+    int samples;
+    
+	double celsius;
+    double fahrenheit;
+    double kelvin;
+};
+
+#endif
